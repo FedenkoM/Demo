e27c12f1e1b0be4ee99c3d825759b6f401311252
FedenkoM <fedenkomisha@gmail.com>
2018-11-07 23:48:08 +0200

    First commit
    Signed-off-by: FedenkoM <fedenkomisha@gmail.com>

---
+++ Demo/dbo/Tables/StockChanges.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [dbo].[StockChanges] (
+    [MedicineId] INT      NOT NULL,
+    [DocID]      INT      NOT NULL,
+    [DocType]    TINYINT  NOT NULL,
+    [ChangeDate] DATETIME NULL,
+    [Quantity]   INT      NOT NULL,
+    CONSTRAINT [PK_StockChanges] PRIMARY KEY CLUSTERED ([MedicineId] ASC, [DocID] ASC, [DocType] ASC)
+);
+
+
+GO
+CREATE NONCLUSTERED INDEX [IX_ChangeDate]
+    ON [dbo].[StockChanges]([ChangeDate] ASC)
+    INCLUDE([Quantity]);
+
